0884cdb96e1db9b9cde7315b70c9d11ad1919c73
MINSOO SHIN <minso0317@naver.com>
2025-02-24 15:24:05 +0900

    :sparkles: CRUD article add CRUD for article

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
+// Products API
+
 app.get("/products", async (req, res) => {
   const products = await prisma.product.findMany();
   res.status(200).send(products);
@@ -40,6 +42,45 @@ app.patch("/products/:id", async (req, res) => {
 app.delete("/products/:id", async (req, res) => {
   const { id } = req.params;
   await prisma.product.delete({
+    where: { id },
+  });
+  res.status(204).send("Success delete");
+});
+
+// Articles API
+
+app.get("/articles", async (req, res) => {
+  const articles = await prisma.article.findMany();
+  res.status(200).send(articles);
+});
+
+app.get("/articles/:id", async (req, res) => {
+  const { id } = req.params;
+  const article = await prisma.article.findUniqueOrThrow({
+    where: { id },
+  });
+  res.status(200).send(article);
+});
+
+app.post("/articles", async (req, res) => {
+  const article = await prisma.article.create({
+    data: req.body,
+  });
+  res.status(201).send(article);
+});
+
+app.patch("/articles/:id", async (req, res) => {
+  const { id } = req.params;
+  const article = await prisma.article.update({
+    where: { id },
+    data: req.body,
+  });
+  res.status(201).send(article);
+});
+
+app.delete("/articles/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.article.delete({
     where: { id },
   });
   res.status(204).send("Success delete");
