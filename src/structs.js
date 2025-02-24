8f05cba89e3c48d17a6066b3553d185ae54999ab
MINSOO SHIN <minso0317@naver.com>
2025-02-24 19:05:08 +0900

    :wrench: structs.js add structs.js

---
+++ src/structs.js
@@ -0,0 +1,17 @@
+import * as s from "superstruct";
+
+export const CreateProduct = s.object({
+  name: s.size(s.string(), 1, 50),
+  description: s.size(s.string(), 1, 100),
+  price: s.min(s.number(), 0),
+  tags: s.size(s.string(), 1, 30),
+});
+
+export const PatchProduct = s.partial(CreateProduct);
+
+export const CreateArticle = s.object({
+  title: s.size(s.string(), 1, 100),
+  content: s.size(s.string(), 1, 30),
+});
+
+export const PatchArticle = s.partial(CreateArticle);
