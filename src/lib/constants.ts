172b0477987c8cc3fbc75555605a49ef9ffbb085
MINSOO SHIN <minso0317@naver.com>
2025-06-03 19:42:21 +0900

    feat: 이미지 aws s3적용

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -10,3 +10,7 @@ export const ACCESS_TOKEN_COOKIE_NAME = 'access-token';
 export const REFRESH_TOKEN_COOKIE_NAME = 'refresh-token';
 export const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET;
 export const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET;
+export const AWS_REGION = process.env.AWS_REGION || '';
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || '';
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || '';
+export const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME || '';
--- src/routers/imageRouter.ts
+++ src/routers/imageRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { upload, uploadImage } from '../controllers/imageController';
 import { withAsync } from '../lib/withAsync';
+import uploadImage from '../controllers/imageController';
+import { uploader } from '../middlewares/uploader';
 
 const imagesRouter = express.Router();
 
-imagesRouter.post('/upload', upload.single('image'), withAsync(uploadImage));
+imagesRouter.post('/upload', uploader.single('image'), withAsync(uploadImage));
 
 export default imagesRouter;
