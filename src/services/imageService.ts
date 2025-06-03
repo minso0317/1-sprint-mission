3e5ce8758201dfd19a85f1dedcb4964797ab12d2
MINSOO SHIN <minso0317@naver.com>
2025-06-03 20:11:52 +0900

    refactory: s3client 분리

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,20 +1,8 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import {
-  AWS_ACCESS_KEY_ID,
-  AWS_BUCKET_NAME,
-  AWS_REGION,
-  AWS_SECRET_ACCESS_KEY,
-} from '../lib/constants';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { AWS_BUCKET_NAME, AWS_REGION } from '../lib/constants';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
-
-const s3 = new S3Client({
-  region: AWS_REGION,
-  credentials: {
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  },
-});
+import { s3Client } from '../lib/s3Client';
 
 export async function uploadBufferToS3(
   buffer: Buffer,
@@ -31,7 +19,7 @@ export async function uploadBufferToS3(
     ContentType: mimetype,
   });
 
-  await s3.send(command);
+  await s3Client.send(command);
 
   return `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${key}`;
 }
