576298fdce551191977879dc2f15968d5e8585a6
MINSOO SHIN <minso0317@naver.com>
2025-06-15 00:37:30 +0900

    feat: Dockerfile, docker-compose.yaml 구현

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { app } from './app';
+import { PORT } from './lib/constants';
 
-app.listen(3000, () => {
+app.listen(PORT || 3000, () => {
   console.log('Server is running on port 3000');
 });
